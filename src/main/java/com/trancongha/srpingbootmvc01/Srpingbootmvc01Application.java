2920f2c2eac4295614a49c01eef720c7784e5502
Tran Cong Ha <trancong2003111@gmail.com>
2024-12-16 21:22:19 +0700

    Step 1: Create Project Srping Boot MVC

---
+++ src/main/java/com/trancongha/srpingbootmvc01/Srpingbootmvc01Application.java
@@ -0,0 +1,13 @@
+package com.trancongha.srpingbootmvc01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Srpingbootmvc01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Srpingbootmvc01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/trancongha/srpingbootmvc01/Srpingbootmvc01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trancongha.srpingbootmvc01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Srpingbootmvc01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
